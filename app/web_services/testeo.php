0042c522a881f15f3e71563364584e01abcecee5
zuki26 <nelson-araoz@hotmail.com>
2014-12-17 19:30:36 -0400

    Nusoap anadido

---
+++ app/web_services/testeo.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zuki
+ * Date: 12/17/14
+ * Time: 7:18 PM
+ */
+include_once('lib/nusoap.php');
+
+$server = new soap_server();
+$server->configureWSDL('demo', 'urn:demo');
+
+
+
+$server->register('estapunto',
+    array('x' => 'xsd:string','y' => 'xsd:string','gid' => 'xsd:string'),
+    array('resp' => 'xsd:string'),
+    'xsd:demo');
+function estapunto($x,$y,$gid)
+{
+    return "tu vieja";
+}
+
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA)
+    ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
