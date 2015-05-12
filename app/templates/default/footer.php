8f3689d61901c26f708fd8122c5e95261f43feeb
diegoRodriguezAguila <diroag@gmail.com>
2015-05-12 09:14:03 -0400

    Footer sin propaganda

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -2,7 +2,5 @@
 </div>
 <div class="col-lg-2"></div>
 </div>
-
-<div id="yzmpa_footer_inner"><iframe style="width: 100%; height: 100%; border: 0;padding:0;margin:0;" frameborder="0" allowtransparency="yes" scrolling="no" src="//ext1.engageya.com/widget/inj_footer_tag_amonetize.html" s3319280894136560439="true" replaced="true"></iframe></div>
 </body>
 </html>
